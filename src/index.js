ce7be9522c57598c076e94f3363a419fe07c6e23
DenysTkachov <D.Tkachev@ukr.net>
2023-09-28 18:00:15 +0200

    566

--- src/index.js
+++ src/index.js
@@ -10,40 +10,68 @@ console.log(refs);
 
 const { breedSelector, loader, error, catInfo } = refs;
 
-loader.classList.replace('loader', 'is-hidden');
+
+loader.classList.remove('loader');
 error.classList.add('is-hidden');
+catInfo.innerHTML = '';
 
-fetchBreeds()
-    .then(breedsArray => {
-        const dataProvider = breedsArray.map(({ id, name }) => {
-            return { value: id, text: name };
-        });
 
-        dataProvider.unshift({
-            value: '',
-            text: '-- select a breed --',
-            disabled: true,
-        });
-        
-        new SlimSelect({
-            select: breedSelector,
-            data: dataProvider,
-        });
-        
-    })
-    .catch(onFetchError);
+const requestOptions = {
+  method: 'GET',
+  headers: {
+    'Content-Type': 'application/json',
+  },
+};
+
+
+const selectOptions = {
+  placeholder: '-- select a breed --',
+};
+
 
 function onFetchError(errorInstance) {
-    loader.classList.replace('loader', 'is-hidden');
-    catInfo.innerHTML = '';
+  loader.classList.add('is-hidden');
+  catInfo.innerHTML = '';
 
-    Notify.failure(
-        'Oops! Something went wrong! Try reloading the page or select another cat breed!',
-        {
-            timeout: 5000,
-        }
-        );
+  Notify.failure(
+    'Oops! Something went wrong! Try reloading the page or select another cat breed!',
+    {
+      timeout: 5000,
+    }
+  );
 }
+
+
+fetch('https://api.thecatapi.com/v1/breeds', requestOptions)
+  .then(response => {
+    if (!response.ok) {
+      throw new Error('Network response was not ok');
+    }
+    return response.json();
+  })
+  .then(breedsArray => {
+    loader.classList.add('is-hidden');
+
+    const dataProvider = breedsArray.map(({ id, name }) => ({
+      value: id,
+      text: name,
+    }));
+
+    dataProvider.unshift({
+      value: '',
+      text: '-- select a breed --',
+      disabled: true,
+    });
+
+    new SlimSelect(breedSelector, {
+      data: dataProvider,
+      ...selectOptions,
+    });
+  })
+  .catch(errorInstance => {
+    onFetchError(errorInstance);
+    console.error('Error:', errorInstance);
+  });
 
 
 

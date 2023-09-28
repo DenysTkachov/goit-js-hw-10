c6b4836fc819d61c867ff584487e2c97351485b0
DenysTkachov <D.Tkachev@ukr.net>
2023-09-28 18:28:26 +0200

    1234

--- src/index.js
+++ src/index.js
@@ -10,10 +10,10 @@ console.log(refs);
 
 const { breedSelector, loader, error, catInfo } = refs;
 
-
 loader.classList.remove('loader');
 error.classList.add('is-hidden');
 catInfo.innerHTML = '';
+
 
 
 const requestOptions = {
@@ -23,11 +23,9 @@ const requestOptions = {
   },
 };
 
-
 const selectOptions = {
   placeholder: '-- select a breed --',
 };
-
 
 function onFetchError(errorInstance) {
   loader.classList.add('is-hidden');
@@ -40,7 +38,6 @@ function onFetchError(errorInstance) {
     }
   );
 }
-
 
 fetch('https://api.thecatapi.com/v1/breeds', requestOptions)
   .then(response => {
@@ -63,15 +60,54 @@ fetch('https://api.thecatapi.com/v1/breeds', requestOptions)
       disabled: true,
     });
 
+    
     new SlimSelect(breedSelector, {
       data: dataProvider,
       ...selectOptions,
     });
   })
-  .catch(errorInstance => {
+  .catch(errorInstance => {   
     onFetchError(errorInstance);
     console.error('Error:', errorInstance);
   });
+
+
+
+
+
+
+
+// fetchBreeds()
+//     .then(breedsArray => {
+//         const dataProvider = breedsArray.map(({ id, name }) => {
+//             return { value: id, text: name };
+//         });
+
+//         dataProvider.unshift({
+//             value: '',
+//             text: '-- select a breed --',
+//             disabled: true,
+//         });
+        
+//         new SlimSelect({
+//             select: breedSelector,
+//             data: dataProvider,
+//         });
+        
+//     })
+//     .catch(onFetchError);
+
+// function onFetchError(errorInstance) {
+//     loader.classList.replace('loader', 'is-hidden');
+//     catInfo.innerHTML = '';
+
+//     Notify.failure(
+//         'Oops! Something went wrong! Try reloading the page or select another cat breed!',
+//         {
+//             timeout: 5000,
+//         }
+//         );
+// }
 
 
 
